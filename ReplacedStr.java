6e5601198ea4d7ed4c390356884ad4801e27140d
LollatiSindhuja <sindhujalollati@gmail.com>
2025-01-26 18:47:03 +0530

    Add files via upload

---
+++ ReplacedStr.java
@@ -0,0 +1,8 @@
+public class ReplacedStr {
+    public static void main(String[] args) {
+        String str="Hello world";
+        String replacedstr=str.replace('l','p');
+        System.out.println("replaced string"+replacedstr);
+    }
+    
+}
